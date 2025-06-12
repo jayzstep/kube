79fb1838e0a58ff125433fb253d4bb8c76b7dd54
Jasse <jasse.merivirta@helsinki.fi>
2025-06-12 21:02:13 +0300

    1.07 lisää log-outputille ingressi

---
+++ part1/log_output/README.md
@@ -0,0 +1,2 @@
+Käynnistyy komennolla `kubectl apply -f manifests`
+timestamp löytyy: [localhost:8081/status](http://localhost:8081/status)
--- part1/log_output/index.js
+++ part1/log_output/index.js
@@ -1,9 +1,25 @@
-const randomString = Math.random().toString(36).substring(2, 15);
+const express = require('express');
 
-function logMessage() {
+const randomString = Math.random().toString(36).substring(2, 15);
+const app = express();
+const port = process.env.PORT || 3000;
+
+const logMessage = () => {
   const timestamp = new Date().toISOString();
   console.log(`${timestamp}: ${randomString}`);
 }
+
+app.get('/status', (req, res) => {
+  const timestamp = new Date().toISOString();
+  res.json({
+    timestamp,
+    string: randomString
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 setInterval(logMessage, 5000);
 logMessage();

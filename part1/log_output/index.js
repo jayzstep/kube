94aecbe52a0a94af1693d55bdb4bf4c484e40a11
Jasse <jasse.merivirta@helsinki.fi>
2025-07-09 15:14:36 +0300

    2.5 lisää configMap

--- part1/log_output/index.js
+++ part1/log_output/index.js
@@ -7,17 +7,20 @@ const express = require('express')
 const randomString = Math.random().toString(36).substring(2, 15)
 const app = express()
 const port = process.env.PORT || 3000
+const MESSAGE = process.env.MESSAGE || 'no message set'
 
-const directory = path.join('/', 'usr', 'src', 'app', 'files')
-const logFilePath = path.join(directory, 'log.txt')
+const filesDirectory = path.join('/', 'usr', 'src', 'app', 'files')
+const configDirectory = path.join('/', 'usr', 'src', 'app', 'config')
+const logFilePath = path.join(filesDirectory, 'log.txt')
+const informationFilePath = path.join(configDirectory, 'information.txt')
 
 const logMessage = () => {
   const timestamp = new Date().toISOString()
   const message = `${timestamp}: ${randomString}`
   console.log(message)
 
-  if (!fs.existsSync(directory)) {
-    fs.mkdirSync(directory, { recursive: true })
+  if (!fs.existsSync(filesDirectory)) {
+    fs.mkdirSync(filesDirectory, { recursive: true })
   }
 
   fs.writeFileSync(logFilePath, message)
@@ -28,10 +31,11 @@ app.use(express.json())
 app.get('/status', async (req, res) => {
   try {
     const log = fs.readFileSync(logFilePath, 'utf8')
+    const information = fs.readFileSync(informationFilePath, 'utf8')
     const response = await axios.get(
       'http://ping-pong-svc.exercises:2345/pings'
     )
-    const finalMessage = `${log}.\n Ping / Pongs: ${response.data.counter}`
+    const finalMessage = `file content: ${information} \n env variable: MESSAGE=${MESSAGE}\n${log}.\n Ping / Pongs: ${response.data.counter}`
     res.send(finalMessage)
   } catch (error) {
     res.status(500).send('Something went wrong: ', error)

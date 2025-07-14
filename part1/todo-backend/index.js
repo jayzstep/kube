6bbd2e1b7d042d8092882b6432cf7e46ccf66d2f
Jasse <jasse.merivirta@helsinki.fi>
2025-07-14 13:23:42 +0300

    lisää tietokanta backendiin

--- part1/todo-backend/index.js
+++ part1/todo-backend/index.js
@@ -1,56 +1,59 @@
 const express = require('express')
 const cors = require('cors')
+const { Pool } = require('pg')
+
 const app = express()
 
 const PORT = process.env.PORT || 3000
+const pool = new Pool({ connectionString: process.env.DB_URI })
 
 app.use(cors())
 app.use(express.json())
 
-const genId = () => Math.floor(Math.random() * 1000000)
-
-const todos = [
-  {
-    id: 1,
-    text: 'Vie roskat',
-    completed: false,
-    createdAt: '2025-06-10T10:00:00Z'
-  },
-  {
-    id: 2,
-    text: 'Soita Donna Lee',
-    completed: true,
-    createdAt: '2025-06-09T14:30:00Z'
-  },
-  {
-    id: 3,
-    text: 'Viritä basso',
-    completed: false,
-    createdAt: '2025-06-05T09:15:00Z'
+const initDatabase = async () => {
+  try {
+    console.log('Connecting to database...')
+    await pool.query(`CREATE TABLE IF NOT EXISTS todos (
+      id SERIAL PRIMARY KEY,
+      text VARCHAR(140) NOT NULL,
+      completed BOOLEAN NOT NULL DEFAULT false,
+      createdAt TIMESTAMP NOT NULL DEFAULT now()
+    )`)
+    console.log('Database connected successfully')
+  } catch (error) {
+    console.error('Database connection failed:', error)
+    throw error
   }
-]
+}
 
-app.get('/api/todos', (req, res) => {
-  res.json(todos)
+app.get('/api/todos', async (req, res) => {
+  try {
+    const todos = await pool.query(`SELECT * FROM todos`)
+    res.status(200).json(todos.rows)
+  } catch (error) {
+    console.error('Database error:', error)
+    res.status(500).send('Database error')
+  }
 })
 
-app.post('/api/todos', (req, res) => {
+app.post('/api/todos', async (req, res) => {
   if (!req.body.data || req.body.data.length > 140) {
     return res.status(400).json({
       error: 'Todo text is required and cannot be longer than 140 characters'
     })
   }
-
-  const newTodo = {
-    id: genId(),
-    text: req.body.data,
-    completed: false,
-    createdAt: new Date().toISOString()
+  try {
+    const text = 'INSERT INTO todos (text) VALUES($1) RETURNING *'
+    const values = [req.body.data]
+    const result = await pool.query(text, values)
+    res.status(201).json(result.rows[0])
+  } catch (error) {
+    console.error('Database error:', error)
+    res.status(500).send('Database error')
   }
-  todos.push(newTodo)
-  res.status(201).json(newTodo)
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await initDatabase()
   console.log(`Server started in port ${PORT}`)
 })
